7f068ffcd24593c59178fba10208188668acaf4b
Jack Delahunt <20085409@mail.wit.ie>
2020-07-23 13:55:48 +0100

    added better chunk loading and unloading

--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -14,38 +14,66 @@ public class World : MonoBehaviour
 	{
 		chunks = new Dictionary<int, Chunk>();
 		player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+
+		// call the chunkloader every second
+		InvokeRepeating("updateChunksToLoad", 0f, 1f);
 	}
 
-	private void Update()
-	{
-		updateChunksToLoad();
-	}
 
 	public void updateChunksToLoad()
 	{
+		//create a blank chunk list
+		List<Chunk> newChunks = new List<Chunk>();
 		for (int i = ChunkData.getRealtiveChunkCoord(player.transform.position) - (ChunkData.renderDisctance * ChunkData.chunkWidth); i <= ChunkData.getRealtiveChunkCoord(player.transform.position) + (ChunkData.renderDisctance * ChunkData.chunkWidth); i += ChunkData.chunkWidth)
 		{
-			generateChunk(i);
+			// get the chunk at this position
+			Chunk newLoadedChunk = generateChunk(i);
+
+			// set it to active if it is not
+			newLoadedChunk.gameObject.SetActive(true);
+
+			// add this chunk to the newly loaded chunks
+			newChunks.Add(newLoadedChunk);
 		}
+
+		// go through all the loaded chunks in the last frame
+		for(int i = 0; i < currentLoadedChunks.Count; i++)
+		{
+			// if the chunk is not in the new list of chunks to
+			// load then set active to false (unloading)
+			if (!newChunks.Contains(currentLoadedChunks[i]))
+				currentLoadedChunks[i].gameObject.SetActive(false);
+		}
+
+		// set the current loaded chunks to the new list of chunks
+		currentLoadedChunks = newChunks;
 	}
 
-	public void generateChunk(int x)
+	public Chunk generateChunk(int x)
 	{
+		// create a blank template for this chunk
 		Chunk found;
-		// check if the chunk already exists
-		if (!chunks.TryGetValue(x, out found)) // if it does not generate a new chunk
+		// check if the chunk already exists if it does then set found to it
+		// if the chunk does not exist then generate a new chunk
+		if (!chunks.TryGetValue(x, out found)) 
 		{
-			Chunk addingChunk = Instantiate(chunkPrefab, gameObject.transform).GetComponent<Chunk>();
-			addingChunk.chunkX = x;
-			addingChunk.chunkY = 0;
-			chunks.Add(addingChunk.chunkX, addingChunk);
-			currentLoadedChunks.Add(addingChunk);
+			found = Instantiate(chunkPrefab, gameObject.transform).GetComponent<Chunk>();
+			found.chunkX = x;
+			found.chunkY = 0;
+
+			// add this new chunk the chunk dictionary
+			chunks.Add(found.chunkX, found);
 		}
+
+		// return either the newly generated chunk or the already created chunk
+		return found;
 	}
 
 	public Chunk getChunk(Vector3 worldPosition)
 	{
 		Chunk found;
+		
+		// get what chunk is at this x position, if there is none then it is null
 		chunks.TryGetValue(ChunkData.getRealtiveChunkCoord(worldPosition), out found);
 		return found;
 	}
