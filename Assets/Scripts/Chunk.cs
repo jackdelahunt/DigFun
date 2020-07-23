72c959b7b6900133b41875b6db004f0f3aeb3843
Jack Delahunt <20085409@mail.wit.ie>
2020-07-23 18:55:01 +0100

    added terrain generation

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -19,35 +19,52 @@ public class Chunk : MonoBehaviour
 
 	public void addTile(Vector3Int pos, Tile tile)
 	{
-		if (pos.y < ChunkData.chunkHeight - 5)
+		if (tilemap.GetTile(pos) == null)
 		{
-			tilemap.SetTile(pos, tiles[2]);
-			return;
+			if(tile == null)
+				tilemap.SetTile(pos, tiles[0]);
+			else
+				tilemap.SetTile(pos, tile);
 		}
-
-		if (tilemap.GetTile(pos + new Vector3Int(0, 1, 0)) == null)
-			tilemap.SetTile(pos, tiles[1]);
-		else
-			tilemap.SetTile(pos, tiles[0]);
-
-		if(tilemap.GetTile(pos + new Vector3Int(0, -1, 0)) != null)
-			tilemap.SetTile(pos + new Vector3Int(0, -1, 0), tiles[0]);
 	}
 
     public void removeTile(Vector3Int pos) 
 	{
         tilemap.SetTile(pos, null);
-
-		if (tilemap.GetTile(pos + new Vector3Int(0, -1, 0)) != null)
-			tilemap.SetTile(pos + new Vector3Int(0, -1, 0), tiles[1]);
 	}
 
 	public void generateStartTerrain()
 	{
-		for (int y = chunkY; y < chunkY + ChunkData.chunkHeight; y++) {
+		// these values will be set by the biome in the future
+		int terrainHeight = 20;
+		float scale = 0.1f;
+		// generate the cave structure
+		// chunkHeight - terrainHeight = undergroundHeight??
+		for (int y = chunkY; y < chunkY + ChunkData.chunkHeight - terrainHeight; y++) {
 			for (int x = chunkX; x < chunkX + ChunkData.chunkWidth; x++)
 			{
-				addTile(new Vector3Int(x, y, 0), null);
+				if (!Noise.caveNoise(x, y, 0.4f, 0.15f, 0))
+					continue;
+
+				if (y == ChunkData.chunkHeight - 1) 
+					addTile(new Vector3Int(x, y, 0), tiles[1]);
+				else if (y > ChunkData.chunkHeight - 5)
+					addTile(new Vector3Int(x, y, 0), tiles[0]);
+				else
+					addTile(new Vector3Int(x, y, 0), tiles[2]);
+
+			}
+		}
+
+		// generate the above terrain
+		for(int x = chunkX; x < chunkX + ChunkData.chunkWidth; x++)
+		{
+			float noise = Noise.terrainNoise(x, terrainHeight, scale, 100);
+			int heightOfTerrainHere = Mathf.FloorToInt(terrainHeight * noise);
+
+			for (int y = 0; y < heightOfTerrainHere; y++)
+			{
+				addTile(new Vector3Int(x, ChunkData.chunkHeight - terrainHeight + y, 0), tiles[0]);
 			}
 		}
 	}
--- Assets/Scripts/Noise.cs
+++ Assets/Scripts/Noise.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public static class Noise
 {
     
+    public static float terrainNoise(int x, int y, float scale, int offset)
+	{
+		return Mathf.PerlinNoise((x * scale) + offset, (y * scale) + offset);
+	}
 
+	public static bool caveNoise(int x, int y, float threshold, float scale, int offset)
+	{
+		return Mathf.PerlinNoise((x * scale) + offset, (y * scale) + offset) > threshold;
+	}
 
 }
